49ab3b015557c81d0ed71f70505241a04fd5a7d6
Christian <christian.balazs@yahoo.com>
2021-10-01 13:21:12 +0100

    next commit on test file

--- mytest.py
+++ mytest.py
@@ -1,1 +1,30 @@
 print("I can python")
+
+variableForAnInput = input("Enter A Value to be put in the variable: ")
+
+
+
+variableForAString = "this is a string"
+variableForAnotherString = "this is another string"
+
+variableForAFloat = 15.2
+
+variableForAnInteger = 121
+
+variableForABoolean = True
+
+#print(variableForAString)
+#print(variableForAFloat)
+
+#print(2+2)
+
+#print(variableForAString + variableForAnotherString)
+
+#print(variableForAFloat + variableForAnInteger)
+
+#print(variableForAnotherString + str(variableForAFloat))
+
+#print(f"some text here with {variableForAnInteger} lessons")
+
+#print (variableForAnInput)
+
