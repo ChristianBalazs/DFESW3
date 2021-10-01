b0be584f9fe67141b6e67d94bd57814daa7e9c07
Christian <christian.balazs@yahoo.com>
2021-10-01 13:21:51 +0100

    first commit on exercise2

---
+++ exercise2.py
@@ -0,0 +1,24 @@
+# 1. What will be the output of this code?
+
+number1 = input("Enter whole number: ")
+number2 = input("Enter decimal number: ")
+
+integer_number = int(number1)
+float_number = float(number2)
+round_number = int(round(float_number))
+
+print(number1)
+print(number2)
+print(round_number)
+
+
+# Answer: 
+# ask for whole number input
+# ask for decimal number input
+# print number1 (whole no) 
+# print number2 (decimal no)
+# take number2 and convert into float type (round up if .5 or more)
+# print round_number as a integer of number2 which has been converted into float beforehand
+
+#Correct answer
+
