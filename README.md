8bb1caecc0355c409bb6dfe8ce91e0b16fa332ca
Christian <christian.balazs@yahoo.com>
2021-10-01 10:03:04 +0100

    new commit fromVS Code

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 random stuff for this course
 first line from the GIT bash on my pc 
 HERE - another comment 
+
+Add a new line to the README from VS Code.
---
+++ SQL/Products_table_commands.sql
@@ -0,0 +1,9 @@
+INSERT INTO sql_work1.products_table (ProductCategory, ProductName, UserMinAge, ProductWarranty, FreeDelivery) 
+VALUES
+('toys', 'puzzle Dog', '7', '2', 'Y'),
+('clothes', ' t-shirt Superman', '7', '0', 'N'),
+('toys', 'RC car BatMobile', '10', '1', 'Y'),
+('houseshold', 'IKea kitchen Knife ', '18', '3', 'N'),
+('household', 'AMK hair dryer', '18', '3', 'Y');
+
+SELECT * FROM sql_work1.products_table;
