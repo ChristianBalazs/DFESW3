9358ed08e46ed0cee5137fd5e08e60c1b64a9f0c
Christian <christian.balazs@yahoo.com>
2021-10-04 15:21:02 +0100

    commit

---
+++ w3resource_exercises.py
@@ -0,0 +1,16 @@
+
+# 1. Write a Python program to find those numbers which are divisible by 7 and multiple of 5, between 1500 and 2700 (both included)
+
+#varNum = range(1500, 2700,)
+
+# for num in range(1500, 2700,20):
+#     print(num)
+
+# Doesn't work 
+# for num in range(1500, 2700,50):
+#     while num % 7 == 0 and num % 5 == 0:
+#         print(num)
+
+
+
+
