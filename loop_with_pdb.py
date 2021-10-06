91ba3aad3078f9d6468a2bfd2287bde5fd7db78b
Christian <christian.balazs@yahoo.com>
2021-10-06 09:35:38 +0100

    com

---
+++ loop_with_pdb.py
@@ -0,0 +1,14 @@
+
+
+import pdb
+
+pdb.set_trace()
+
+booksVar = ['Winnie the Pooh', 'War and Peace', '1984', 'My family and other animals', 'Boy', 'The Art of War', 'Of Mice and Men', 'Peter and James']
+
+for bookItem in booksVar:
+    if bookItem == '1984':
+        print(bookItem + " is the only book in the list that can be cast into a number")
+    else:
+        print(bookItem)
+
