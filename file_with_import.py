647e02ec9eddc7f83da0a0ae4dbcddb16b0f7dda
Christian <christian.balazs@yahoo.com>
2021-10-06 09:12:21 +0100

    com

--- file_with_import.py
+++ file_with_import.py
@@ -1,10 +1,13 @@
 
-# import functions
+# import functions - one by one by using "from <filename> import<function name>"
 
 
 from functions_file import max_num
-
 print(max_num(12,45,9))
 
 
+from functions_file import add_calc
+print(add_calc(3,5))
+
+# or we can import all functions from the file using " from <filename> import * "
 
